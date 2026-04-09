c37642ec8b1c513703d7d50ffdeadca33f9ed14b
EytleBB <3035986089@qq.com>
2026-04-09 18:28:04 +0800

    chore: remove portable dist dirs from git, fix corrupt objects

--- checkpoints/smoke/portable_verify/launch_run_download.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo === CS Prophet Portable (download-only) ===
-set "PYTHONNOUSERSITE=1"
-"%~dp0dist\python\python.exe" "%~dp0run_download.py" %*
-if errorlevel 1 pause
--- checkpoints/smoke/portable_verify/launch_run_pipeline.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo === CS Prophet Portable (pipeline) ===
-set "PYTHONNOUSERSITE=1"
-"%~dp0dist\python\python.exe" "%~dp0run_pipeline.py" %*
-if errorlevel 1 pause
